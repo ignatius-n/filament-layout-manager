de2202cdc4cadf8dde20db95f72e3eceb64d4830
asosick <57464533+asosick@users.noreply.github.com>
2025-01-28 03:13:21 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Asosick\ReorderWidgets\Tests;
 
+use Asosick\ReorderWidgets\ReorderWidgetsServiceProvider;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Asosick\ReorderWidgets\ReorderWidgetsServiceProvider;
 
 class TestCase extends Orchestra
 {
